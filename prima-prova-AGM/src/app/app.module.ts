cc42edb964b898ad0027a015006000de996bef3e
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-04-03 17:42:52 +0000

    Fine 01

--- prima-prova-AGM/src/app/app.module.ts
+++ prima-prova-AGM/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgmCoreModule } from '@agm/core'; //importa questa libreria
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        // Imposta qui le tue api key
+        AgmCoreModule.forRoot({ apiKey: 'AIzaSyCSitr1LMQInYxrNd9md0idPqYtUhWB1dM' }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
